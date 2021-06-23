e86eddffa4ca1cd8aa85ca0120997ed084150382
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-06-23 14:02:12 +0530

    added addition and subtraction methods

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('hello!')
+function add(inputArr) {
+  let result = 0;
+  for (let i = 0; i < inputArr.length; i++) {
+    result = result + inputArr[i];
+  }
+  return result;
+}
+
+function subtract(inputArr) {
+  let result = inpArr[0];
+  for (let i = 1; i < inputArr.length; i++) {
+    result = result - inputArr[i];
+  }
+  return result;
+}
+
+let inpArr = [3, 4, 9];
+addn = add(inpArr);
+subtn = subtract(inpArr);
+console.log({ addn });
+console.log({ subtn });
