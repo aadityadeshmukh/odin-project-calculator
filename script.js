dc5bfb631c484ecdb950afb76b89be6874b94185
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-06-23 14:03:52 +0530

    multiply method

--- script.js
+++ script.js
@@ -14,8 +14,18 @@ function subtract(inputArr) {
   return result;
 }
 
-let inpArr = [3, 4, 9];
+function multiply(inputArr) {
+  let result = 1;
+  for (let i = 0; i < inputArr.length; i++) {
+    result = result * inputArr[i];
+  }
+  return result;
+}
+
+let inpArr = [3, 4, 9, 8];
 addn = add(inpArr);
 subtn = subtract(inpArr);
+multn = multiply(inpArr);
 console.log({ addn });
 console.log({ subtn });
+console.log({ multn });
