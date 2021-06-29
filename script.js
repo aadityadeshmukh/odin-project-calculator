37eaf8ad173b8e1a8c10d140b7ee1ad49f01bf39
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-06-29 11:29:23 +0530

    support for floats

--- script.js
+++ script.js
@@ -81,6 +81,11 @@ function operateExpression() {
   let expressionStr = screen.textContent;
   let numberArr = expressionStr.split(/[+]+|[-]+|[*]+|[/]+/);
   let operatorArr = expressionStr.split(/[0-9]+|[0-9]+|[0-9]+|[0-9]+/);
+  operatorArr = operatorArr.filter(ch => ch !== '.');
+  // let iDotOperators = operatorArr
+  //   .map((ch, i) => (ch === '.' ? i : -1))
+  //   .filter(index => index !== -1);
+  // console.table({ iDotOperators });
   console.table({ numberArr });
   console.table({ operatorArr });
   let result = +numberArr[0];
